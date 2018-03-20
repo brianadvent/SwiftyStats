d3b332499757d8af1b59655b35d64f8426edfd74
Brian Advent <youtube@brianadvent.com>
2018-03-20 14:46:13 +0100

    Add Chart Creation

--- SwiftyStats/ViewController.swift
+++ SwiftyStats/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    @IBOutlet weak var iosStepper: UIStepper!
+    @IBOutlet weak var macStepper: UIStepper!
+    
+    var iosDataEntry = PieChartDataEntry(value: 0)
+    var macDataEntry = PieChartDataEntry(value: 0)
+    
+    var numberOfDownloadsDataEntries = [PieChartDataEntry]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pieChart.chartDescription?.text = ""
+        
+        iosDataEntry.value = iosStepper.value
+        iosDataEntry.label = "iOS"
+        
+        macDataEntry.value = macStepper.value
+        macDataEntry.label = "macOS"
+        
+        numberOfDownloadsDataEntries = [iosDataEntry, macDataEntry]
+        
+        updateChartData()
+        
+    }
+ 
+    @IBAction func changeiOS(_ sender: UIStepper) {
+        iosDataEntry.value = sender.value
+        updateChartData()
+    }
+    
+    @IBAction func changeMac(_ sender: UIStepper) {
+        macDataEntry.value = sender.value
+        updateChartData()
+    }
+    
+    
+    func updateChartData() {
+        
+        let chartDataSet = PieChartDataSet(values: numberOfDownloadsDataEntries, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor(named:"iosColor"), UIColor(named:"macColor")]
+        chartDataSet.colors = colors as! [NSUIColor]
+        
+        pieChart.data = chartData
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
